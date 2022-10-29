e3e0c8f20b4956d08fb8245d2f85d62b88c031c8
youarenomatchforme <ghadoorah07@gmail.com>
2022-10-29 19:46:05 +0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function BMI(weight, height) {
+  let result = weight / (height * height);
+  return result;
+}
 
+console.log(BMI(40, 166));
+
+function Status(BodyMass) {
+  if (BodyMass < 18.5) {
+    return "You're underwight";
+  } else if (BodyMass >= 18.5 && BodyMass <= 25) {
+    return "You're just right";
+  } else {
+    return "You're over weight";
+  }
+}
+function calculate() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+
+  let bmi_value = BMI(weight, height);
+
+  let desc = Status(bmi_value);
+
+  let box = document.getElementById("result");
+
+  box.innerText = bmi_value + " == " + desc;
+}
